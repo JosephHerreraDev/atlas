e1199d48b6faac9c7e69821f616493be9e2b3168
Joseph Herrera <josephherreraleon@gmail.com>
2026-09-15 14:04:44 -0600

    Nix: add base configuration for hyprland system, home manager, flake

---
+++ configuration.nix
@@ -0,0 +1,56 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  hardware.nvidia.modesetting.enable = true;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "atlas";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/Mexico_City";
+
+  services.xserver.enable = true;
+
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = false;
+  };
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    withUWSM = true; 
+  };
+
+  users.users.joe = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    brave
+    git
+    btop
+    vim
+    fastfetch
+    spotify
+    wget
+  ];
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  system.stateVersion = "26.05";
+}
+
---
+++ home.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "joe";
+  home.homeDirectory = "/home/joe";
+  home.stateVersion = "26.05";
+  
+  imports = [
+    ./modules/theme.nix
+  ];
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      nrs = "sudo nixos-rebuild switch";
+    };
+    initExtra = ''
+       export PS1='\[\e[38;5;76m\]\u\[\e[0m\] in \[\e[38;5;32m\]\w\[\e[0m\] \\$ '
+    '';
+  };
+
+  programs.kitty = {
+    enable = true;
+    settings = {
+      background_opacity = 0.9;
+    };
+  };
+  
+  # wayland.windowManager.hyprland.enable = true;
+  # home.file.".config/hypr".source = /home/joe/dotfiles/hypr;
+}
+
