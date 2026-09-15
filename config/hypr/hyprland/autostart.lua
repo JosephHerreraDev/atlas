f247efc353284e49f637ee4fbaedd0c2e31da010
Joseph Herrera <josephherreraleon@gmail.com>
2026-09-15 16:50:14 -0600

    Hyprland: fix autostart config

--- config/hypr/hyprland/autostart.lua
+++ config/hypr/hyprland/autostart.lua
@@ -1,7 +1,7 @@
 -------------------
 ---- AUTOSTART ----
 -------------------
-hl.on("hyprland.start", function () 
+hl.on("hyprland.start", function()
   hl.exec_cmd("quickshell")
   hl.exec_cmd("hyprpaper")
 --  hl.exec_cmd("bash -lc 'prometheus-clipboard-watch restart'")
