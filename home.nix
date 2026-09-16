dd4684638070a742ed136504ee38514d9cf160f0
Joseph Herrera <josephherreraleon@gmail.com>
2026-09-15 19:36:53 -0600

    Themes: update home manager to use bin/ folder as executables

--- home.nix
+++ home.nix
@@ -1,7 +1,9 @@
 { config, pkgs, ... }:
 
 let
-  dotfiles = "${config.home.homeDirectory}/.local/share/atlas/config";
+  atlasPath = "${config.home.homeDirectory}/.local/share/atlas";
+  dotfiles = "${atlasPath}/config";
+
   create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
 
   configs = {
@@ -15,31 +17,39 @@ in
   home.username = "joe";
   home.homeDirectory = "/home/joe";
   home.stateVersion = "26.05";
-  
+
   imports = [
     ./modules/theme.nix
     ./modules/wallpapers.nix
   ];
 
-
   home.packages = with pkgs; [
     kitty
   ];
 
+  home.sessionVariables = {
+    ATLAS_PATH = atlasPath;
+  };
+
+  home.sessionPath = [
+    "${atlasPath}/bin"
+  ];
+
   programs.bash = {
     enable = true;
+
     shellAliases = {
       nrs = "sudo nixos-rebuild switch --impure --flake ~/.local/share/atlas#atlas";
     };
+
     initExtra = ''
-       export PS1='\[\e[38;5;76m\]\u\[\e[0m\] in \[\e[38;5;32m\]\w\[\e[0m\] \\$ '
+      export PS1='\[\e[38;5;76m\]\u\[\e[0m\] in \[\e[38;5;32m\]\w\[\e[0m\] \\$ '
     '';
   };
-  
-  xdg.configFile = builtins.mapAttrs
-	  (name: subpath: {
-	   source = create_symlink "${dotfiles}/${subpath}";
-	   })
-  configs;
-}
 
+  xdg.configFile = builtins.mapAttrs
+    (name: subpath: {
+      source = create_symlink "${dotfiles}/${subpath}";
+    })
+    configs;
+}
